e848586e91b9534afb732f869b3a0c1b745fd0c0
Gonçalo Folhas <gtaff2001@gmail.com>
2022-10-25 17:18:59 +0100

    5/11 done

--- webclient/demo/src/main/java/com/example/App.java
+++ webclient/demo/src/main/java/com/example/App.java
@@ -20,7 +20,6 @@ public class App
         .map(s -> {
 
             File log = new File("names_and_birthdates.txt");
-            //File log = new File("log.txt");
             
             try{
             if(log.exists()==false){
@@ -42,26 +41,104 @@ public class App
 
         // ex.2
 
-        ArrayList<Integer> ids = new ArrayList<>();
-
         WebClient.create("http://localhost:8080").get().uri("/student").retrieve().bodyToFlux(Student.class)
-        .map(s -> {
+        .count()
+        .subscribe(v->{
+
             File log = new File("total_students.txt");
-            ids.add(s.getId());
             try{
             if(log.exists()==false){
                     System.out.println("We had to make a new file.");
                     log.createNewFile();
             }
             PrintWriter out = new PrintWriter(log);
-            out.append("Total Students: " + ids.size());
+            out.append("Total Students: " + v);
             out.close();
             }catch(IOException e){
                 System.out.println("COULD NOT LOG!!");
             } 
-            return s;
-        })
-        .subscribe();       
+        });       
+
+
+
+        // ex.3
+
+        
+
+        WebClient.create("http://localhost:8080").get().uri("/student").retrieve().bodyToFlux(Student.class)
+        .filter(s -> s.getCredits()!=180 )
+        .count()
+        .subscribe(v -> {
+            File log = new File("active_students.txt");
+            try{
+            if(log.exists()==false){
+                    System.out.println("We had to make a new file.");
+                    log.createNewFile();
+            }
+            PrintWriter out = new PrintWriter(log);
+            out.append("Active Students: " + v);
+            out.close();
+            }catch(IOException e){
+                System.out.println("COULD NOT LOG!!");
+            } 
+        }); 
+
+
+         // ex.4
+
+         WebClient.create("http://localhost:8080").get().uri("/student").retrieve().bodyToFlux(Student.class)
+         .map(s -> {
+ 
+             File log = new File("total_courses.txt");
+             
+             try{
+             if(log.exists()==false){
+                     System.out.println("We had to make a new file.");
+                     log.createNewFile();
+             }
+             PrintWriter out = new PrintWriter(new FileWriter(log, true));
+             int courses = s.getCredits()/6;
+             String toWrite = s.getName() + " -> " + courses + " courses completed\n";
+             out.append(toWrite);
+             out.close();
+             }catch(IOException e){
+                 System.out.println("COULD NOT LOG!!");
+             }
+             
+             return s;
+         })
+         .subscribe(v -> {
+
+            // if i want i can open file, read everything and then sum all the courses
+         });
+
+
+
+          // ex.5
+
+          WebClient.create("http://localhost:8080").get().uri("/student").retrieve().bodyToFlux(Student.class)
+          .filter(s ->  s.getCredits() < 180 && s.getCredits() >= 120)
+          .sort((s1, s2) -> {
+            return s2.getCredits() - s1.getCredits();
+          })
+          .subscribe(s -> {  
+
+            File log = new File("finalists.txt");
+        
+            try{
+            if(log.exists()==false){
+                    System.out.println("We had to make a new file.");
+                    log.createNewFile();
+            }
+            PrintWriter out = new PrintWriter(new FileWriter(log, true));
+            String toWrite = s.getName() + " | " + s.getBirthdate()  + " | " + s.getCredits() + " credits | " + s.getGrade() + "/20 (grade)\n";
+            out.append(toWrite);
+            out.close();
+            }catch(IOException e){
+                System.out.println("COULD NOT LOG!!");
+            }
+        });
+
 
 
         try {
